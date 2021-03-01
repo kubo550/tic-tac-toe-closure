a352392be64e0e220c0be9d603e7ab503872b698
kubo550 <43968748+kubo550@users.noreply.github.com>
2021-03-01 01:49:46 +0100

    Remove Comments

--- js/app.js
+++ js/app.js
@@ -1,23 +1,20 @@
-import { calculateWinner } from "./calculateWinner";
+import { calculateWinner } from "./calculateWinner.js";
 const init = (squares) => {
     const output = document.querySelector(".output");
     const gameboard = ["", "", "", "", "", "", "", "", ""];
-    const player1 = "X";
-    const player2 = "O";
-    let round = 1;
     let winner = "";
+    let round = 1;
     squares.forEach(s => s.classList.remove("X", "O"));
     return (e, i) => {
         if (gameboard[i] || winner) {
             return;
         }
-        const player = round % 2 ? player1 : player2;
+        const player = round % 2 ? "X" : "O";
         gameboard[i] = player;
-        // @ts-ignore
         e.target.classList.add(player);
         winner = calculateWinner(gameboard);
         if (winner) {
-            return alert(`The Winner is ${winner}`);
+            return console.log(`The Winner is ${winner}`);
         }
         round += 1;
     };
